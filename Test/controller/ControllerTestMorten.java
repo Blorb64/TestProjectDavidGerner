0176fd7b1e5bfca947e2035f2f829030df312f1e
MolleJakobsen <Mortenjakobsen.27@gmail.com>
2025-09-30 09:49:09 +0200

    Controller & DagligFast Test

--- Test/controller/ControllerTestMorten.java
+++ Test/controller/ControllerTestMorten.java
@@ -1,16 +1,119 @@
 package controller;
 
+import ordination.DagligFast;
+import ordination.Lægemiddel;
+import ordination.Patient;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTestMorten {
 
+    LocalDate startDato = LocalDate.of(2025, 9, 26);
+    LocalDate slutDato = LocalDate.of(2025, 9, 26);
+    Patient patient = new Patient("222202-2031", "Morten Jakobsen", 75);
+    Lægemiddel lægemiddel = new Lægemiddel("Ibuprofen", 0.002, 0.003, 0.004, "Gram");
+
     @Test
-    void opretDagligFastOrdination() {
+    void opretDagligFastOrdinationTC1() {
+        slutDato = LocalDate.of(2025, 9, 27);
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                1, 1, 1, 1);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(patient.getOrdinationer().getLast(), test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC2() {
+        slutDato = LocalDate.of(2025, 9, 27);
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                0, 0, 0, 0);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(patient.getOrdinationer().getLast(), test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC3() {
+        slutDato = LocalDate.of(2025, 9, 27);
+        lægemiddel = null;
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                0, 1, 0, 1);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(patient.getOrdinationer().getLast(), test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC4() {
+        lægemiddel = null;
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                1, 0, 1, 0);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(patient.getOrdinationer().getLast(), test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC5() {
+        slutDato = LocalDate.of(2025, 9, 25);
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                0, 1, 0, 1);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(null, test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC6() {
+        slutDato = LocalDate.of(2025, 9, 27);
+        patient = null;
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                0, 0, 0, 0);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(null, test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC7() {
+        slutDato = LocalDate.of(2025, 9, 27);
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                -1, 0, 0, 0);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(null, test);
+    }
+    @Test
+    void opretDagligFastOrdinationTC8() {
+        slutDato = LocalDate.of(2025, 9, 27);
+        DagligFast test = Controller.opretDagligFastOrdination(startDato, slutDato, patient, lægemiddel,
+                2, 2, 2, 2);
+        Lægemiddel lMiddelTest = test.getLaegemiddel();
+        assertEquals(lMiddelTest, lægemiddel);
+        assertEquals(null, test);
     }
 
     @Test
-    void anbefaletDosisPrDøgn() {
+    void anbefaletDosisPrDøgnTC1() {
+        patient = new Patient("222202-2203", "Morten Jakobsen", 24);
+        double result = Controller.anbefaletDosisPrDøgn(patient, lægemiddel);
+        assertEquals(0.048, result);
     }
+    @Test
+    void anbefaletDosisPrDøgnTC2() {
+        patient = new Patient("222202-2203", "Morten Jakobsen", 25);
+        double result = Controller.anbefaletDosisPrDøgn(patient, lægemiddel);
+        assertEquals(0.075, result);
+    }
+    @Test
+    void anbefaletDosisPrDøgnTC3() {
+        patient = new Patient("222202-2203", "Morten Jakobsen", 126);
+        double result = Controller.anbefaletDosisPrDøgn(patient, lægemiddel);
+        assertEquals(0.504, result);
+    }
+    @Test
+    void anbefaletDosisPrDøgnTC4() {
+        patient = new Patient("222202-2203", "Morten Jakobsen", 0);
+        double result = Controller.anbefaletDosisPrDøgn(patient, lægemiddel);
+        assertEquals(null, result);
+    }
+
 }
--- Test/ordination/DagligFastTest.java
+++ Test/ordination/DagligFastTest.java
@@ -1,14 +1,140 @@
 package ordination;
 
+import controller.Controller;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligFastTest {
+    LocalDate startDato = LocalDate.of(2025, 9, 26);
+    LocalDate slutDato = LocalDate.of(2025, 9, 26);
+    Patient patient = new Patient("222202-2031", "Morten Jakobsen", 75);
+    Lægemiddel lægemiddel = new Lægemiddel("Ibuprofen", 0.002, 0.003, 0.004, "Gram");
+    Dosis[] doser = new Dosis[4];
 
     @org.junit.jupiter.api.Test
-    void samletDosis() {
+    void samletDosisTC1() {
+        startDato = LocalDate.of(2025, 9, 26);
+        slutDato = LocalDate.of(2025, 9, 26);
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 0);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 0);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 0);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 0);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.samletDosis();
+        assertEquals(0, result);
+    }
+    @org.junit.jupiter.api.Test
+    void samletDosisTC2() {
+        startDato = LocalDate.of(2025, 9, 26);
+        slutDato = LocalDate.of(2025, 9, 26);
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 1);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 1);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 1);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 1);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.samletDosis();
+        assertEquals(4, result);
+    }
+    @org.junit.jupiter.api.Test
+    void samletDosisTC3() {
+        startDato = LocalDate.of(2025, 9, 26);
+        slutDato = LocalDate.of(2025, 9, 30);
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 1);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 1);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 0);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 1);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.samletDosis();
+        assertEquals(15, result);
+    }
+    @org.junit.jupiter.api.Test
+    void samletDosisTC4() {
+        startDato = LocalDate.of(2025, 9, 26);
+        slutDato = LocalDate.of(2025, 9, 26);
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 2);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 1);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 1);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 1);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.samletDosis();
+        assertEquals("fejl", result);
+    }
+    @org.junit.jupiter.api.Test
+    void samletDosisTC5() {
+        startDato = LocalDate.of(2025, 9, 26);
+        slutDato = LocalDate.of(2025, 9, 26);
+        doser[0] = new Dosis(LocalTime.parse("08:00"), -1);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 0);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 0);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 0);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.samletDosis();
+        assertEquals("fejl", result);
+    }
+    @org.junit.jupiter.api.Test
+    void samletDosisTC6() {
+        startDato = LocalDate.of(2025, 9, 26);
+        slutDato = LocalDate.of(2025, 9, 25);
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 1);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 1);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 1);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 1);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.samletDosis();
+        assertEquals("fejl", result);
     }
 
     @org.junit.jupiter.api.Test
-    void doegnDosis() {
+    void doegnDosisTC1() {
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 0);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 0);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 0);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 0);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.doegnDosis();
+        assertEquals(0, result);
+    }
+    @org.junit.jupiter.api.Test
+    void doegnDosisTC2() {
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 1);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 1);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 1);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 1);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.doegnDosis();
+        assertEquals(4, result);
+    }
+    @org.junit.jupiter.api.Test
+    void doegnDosisTC3() {
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 2);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 0);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 1);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 0);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.doegnDosis();
+        assertEquals(3, result);
+    }
+    @org.junit.jupiter.api.Test
+    void doegnDosisTC4() {
+        doser[0] = new Dosis(LocalTime.parse("08:00"), -1);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 0);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 0);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 0);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.doegnDosis();
+        assertEquals("fejl", result);
+    }
+    @org.junit.jupiter.api.Test
+    void doegnDosisTC5() {
+        doser[0] = new Dosis(LocalTime.parse("08:00"), 2);
+        doser[1] = new Dosis(LocalTime.parse("12:00"), 1);
+        doser[2] = new Dosis(LocalTime.parse("18:00"), 1);
+        doser[3] = new Dosis(LocalTime.parse("22:00"), 1);
+        DagligFast test = new DagligFast(startDato, slutDato, doser);
+        double result = test.doegnDosis();
+        assertEquals("fejl", result);
     }
 }
